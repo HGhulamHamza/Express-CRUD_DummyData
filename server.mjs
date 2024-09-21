4d6209542c8abb49a9ba219aea8349a926fb0dad
HGhulamHamza <khawajahamza566@gmail.com>
2024-09-21 23:05:36 +0500

    Created array as a dummy DB

--- server.mjs
+++ server.mjs
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Dummy database
+let users = [
+  { id: 1, name: 'Ahmed', email: 'ahmed@gmail.com' },
+  { id: 2, name: 'Abbas', email: 'abbas@gmail.com' }
+];
 
 // Start the server
 app.listen(port, () => {
